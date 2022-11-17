92f43ae2bb62f6e63eab12292b80abd66b75b14e
dctalk1234 <stacknathaniel14@gmail.com>
2022-11-16 17:40:53 -0800

    start code repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1>Bootstrap Cards To Component Example</h1>
+      <section className="cards">
+      </section>
+    </div>
+  );
+}
