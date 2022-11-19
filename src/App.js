58e0ccc342b0e09fdbbd252f2da61848f1a4eea6
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-11-19 15:20:23 -0700

    play with styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from './components/Card'
 import cardsArr from './cardData'
+import './styles.css'
 const cards = cardsArr.map((ele, idx) => {
   return <Card title={ele.title} text={ele.text} img={ele.img} url={ele.url} key={idx} />
 })
 export default function App() {
   return (
     <div className="App">
-      <h1>Bootstrap Cards To Component Example</h1>
+      <h1>Fun with React Props and Bootstrap</h1>
       <section className="cards">
         {cards}
       </section>
--- src/cardData.js
+++ src/cardData.js
@@ -14,5 +14,21 @@ export default [
     text:
       'This was like being a pirate and we were looking to bury our treasure. It was so isolated and beautiful. ',
     url: 'https://unsplash.com/s/photos/zakynthos'
+  },
+  {
+    img:
+      'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
+    title: 'The Cliffs of Cinque Terre',
+    text:
+      'Manarola, Italy is a beautiful sea town.',
+    url: 'https://www.ciaoflorence.it/en/page/9'
+  },
+  {
+    img:
+      'https://images.unsplash.com/photo-1541343672885-9be56236302a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    title: 'Budapest',
+    text:
+      'The bridges of Budapest are absolutely stunning. ',
+    url: 'https://unsplash.com/photos/ezUDjYy17EA'
   }
 ];
